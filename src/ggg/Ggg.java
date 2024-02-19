c87db72360cfce620fea4d21cc9ec25c7d4dbb7f
Aditi Sharma <shar1394@sheridancollege.ca>
2024-02-19 12:59:37 -0500

    again hello world

--- src/ggg/Ggg.java
+++ src/ggg/Ggg.java
@@ -15,8 +15,6 @@ public class Ggg {
      */
     public static void main(String[] args) {
         System.out.println("hii world");
-        System.out.println("hii world2");
-        
         // TODO code application logic here
     }
     
